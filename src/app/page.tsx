334083929fd05d509504d98708af51997ce6088f
SETAGI <tamayo0711@hotmail.com>
2023-11-19 15:56:45 -0500

    refactoring the useState initial values creating a new type and adding an id generator fot the image items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,28 @@
 import { useState } from "react";
 import { RandomFox } from "./components/RandomFox"
 
-const randomNumber = (): number => {
-  return Math.floor(Math.random() * 24) + 1;
-}
+const randomNumber = (): number => Math.floor(Math.random() * 24) + 1;
+
+const idGenerator = () => Math.random().toString(36).substr(2, 9);
 
 const image: string = `https://randomfox.ca/images/${randomNumber()}.jpg`
 
+type ImageItem = { id: string, url: string }
+
 export default function Home() {
-  const [images, setImages] = useState<string[]>([
-    `https://randomfox.ca/images/1.jpg`,
-    `https://randomfox.ca/images/2.jpg`,
-    `https://randomfox.ca/images/3.jpg`,
-    `https://randomfox.ca/images/4.jpg`
+  const [images, setImages] = useState<Array<ImageItem>>([
+    { id: idGenerator(), url: `https://randomfox.ca/images/1.jpg` },
+    { id: idGenerator(), url: `https://randomfox.ca/images/2.jpg` },
+    { id: idGenerator(), url: `https://randomfox.ca/images/3.jpg` },
+    { id: idGenerator(), url: `https://randomfox.ca/images/4.jpg` },
   ]);
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Sebastian</h1>
       {
-        images.map((image) => (
-          <div key={image} className="p-4">
-            <RandomFox image={image} />
+        images.map(({ id, url }) => (
+          <div key={id} className="p-4">
+            <RandomFox image={url} />
           </div>)
         )
       }
