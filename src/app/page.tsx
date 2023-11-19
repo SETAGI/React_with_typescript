a457c8efa466e447e886c50cc3d1eea314085e05
SETAGI <tamayo0711@hotmail.com>
2023-11-19 15:30:20 -0500

    Adding state with primitive types and generics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState } from "react";
 import { RandomFox } from "./components/RandomFox"
 
 const randomNumber = (): number => {
@@ -7,10 +9,22 @@ const randomNumber = (): number => {
 const image: string = `https://randomfox.ca/images/${randomNumber()}.jpg`
 
 export default function Home() {
+  const [images, setImages] = useState<string[]>([
+    `https://randomfox.ca/images/1.jpg`,
+    `https://randomfox.ca/images/2.jpg`,
+    `https://randomfox.ca/images/3.jpg`,
+    `https://randomfox.ca/images/4.jpg`
+  ]);
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Sebastian</h1>
-      <RandomFox image={image} />
+      {
+        images.map((image) => (
+          <div key={image} className="p-4">
+            <RandomFox image={image} />
+          </div>)
+        )
+      }
     </main>
   )
 }
