2d396e116cde32ef089fcd69fd30c70c8c181d11
SETAGI <tamayo0711@hotmail.com>
2023-11-21 18:05:44 -0500

    Adding a global type for the function called by onClick action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,26 @@
 'use client';
-import { useState } from "react";
+import { MouseEventHandler, useState } from "react";
 import { RandomFox } from "./components/RandomFox"
 
 const randomNumber = (): number => Math.floor(Math.random() * 24) + 1;
 
 const idGenerator = () => Math.random().toString(36).substr(2, 9);
 
-const image: string = `https://randomfox.ca/images/${randomNumber()}.jpg`
-
 type ImageItem = { id: string, url: string }
 
 export default function Home() {
-  const [images, setImages] = useState<Array<ImageItem>>([
-    { id: idGenerator(), url: `https://randomfox.ca/images/1.jpg` },
-    { id: idGenerator(), url: `https://randomfox.ca/images/2.jpg` },
-    { id: idGenerator(), url: `https://randomfox.ca/images/3.jpg` },
-    { id: idGenerator(), url: `https://randomfox.ca/images/4.jpg` },
-  ]);
+  const [images, setImages] = useState<Array<ImageItem>>([]);
+
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault()
+    const newImage = { id: idGenerator(), url: `https://randomfox.ca/images/${randomNumber()}.jpg` }
+    setImages([...images, newImage])
+  }
+
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Sebastian</h1>
+      <button onClick={addNewFox}> Add new fox </button>
       {
         images.map(({ id, url }) => (
           <div key={id} className="p-4">
